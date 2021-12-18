09c5bbcf987ef0137f27f62eb143c13db1252bbe
ubuntu20.04 <asef18766@gmail.com>
2021-12-18 04:51:11 -0800

    add login interface

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,5 @@
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from os import getenv
-
-line_bot_api = LineBotApi(getenv("LINE_BOT_API"))
-line_webhook = WebhookHandler(getenv("WEBHOOK_HANDLER"))
-
+from .line import *
+__all__ = [
+    'line_bot_api',
+    'line_webhook'
+]
