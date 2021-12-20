f40c0fe37d14d824811fbf153e9c1f4106cd5bed
ryanlinjui <ryanlinjui@gmail.com>
2021-12-21 07:22:05 +0800

    upload

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+LoL-helper-linebot/
+|-- src/
+|   |-- analysis/
+|   |   |-- database/
+|   |   |   |-- source.csv
+|   |   |
+|   |   |-- get_data/
+|   |   |   |-- craweler/
+|   |   |   |   |-- __init__.py
+|   |   |   |   |-- lolmoa.py
+|   |   |   |   |-- twlolstat.py
+|   |   |   |
+|   |   |   |-- form.py
+|   |   |   |-- main.py
+|   |   |
+|   |   |-- __init__.py
+|   |   |-- func.py
+|   |   |-- bot_func.py
+|   |
+|   |-- ui/
+|   |   |-- __init__.py
+|   |   |-- graph.py
+|   |   |-- status_eval.py
+|   |   
+|   |-- user_request.py/
+|   |   |-- __init__.py
+|   |   |-- misc.py
+|   |
+|   |-- utils/
+|   |   |-- imgur/
+|   |   |   |-- __init__.py
+|   |   |   |-- upload.py
+|   |   |
+|   |   |-- __init__.py
+|   |   |-- line.py
+|   |   |-- tmp.py
+|   |
+|   |-- __init__.py
+|   |-- app.py
+|   |-- local_test.sh
+|   |-- Procfile
+|
+|-- docs/
+|   |-- analysis_method.pdf
+|   |-- conf.py
+|
+|-- .gitignore
+|-- requirements.txt
+|-- README.md
