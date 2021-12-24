1d90e623009752c0daa1b6f181cad217fa68adb9
ryanlinjui <ryanlinjui@gmail.com>
2021-12-24 20:15:00 +0800

    upload

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -2,10 +2,12 @@ from access import(
     login,
     is_login,
     logout,
-    get_player_data
+    get_player_data,
+    update_db
 )
 
 def behavior(line_user_id:str,mode:str):
     if mode == "切換玩家":
         return logout(line_user_id)
     return get_player_data(line_user_id,mode)
+update_db()
